0a6da5c30c4d5e35cc9d551ba6da24b435966163
VAlmea <almeavicente@gmail.com>
2022-07-14 14:26:35 -0400

    add doc

--- ToDo.API/Controllers/ToDoController.cs
+++ ToDo.API/Controllers/ToDoController.cs
@@ -29,8 +29,10 @@ namespace ToDo.API.Controllers
         /// <summary>
         /// Get all ToDo items
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A list of todo items</returns>
+        /// <response code="200">Returns the the list of todo</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<ActivityResponseDTO>>> GetAll()
         {
             var activities = await _toDoService.GetAllActivitiesAsync();
@@ -39,9 +41,23 @@ namespace ToDo.API.Controllers
         }
 
         /// <summary>
-        /// Create a ToDo item
+        /// Creates a TodoItem.
         /// </summary>
-        /// <returns></returns>
+        /// <param name="item">The RequestDTO</param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item #1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -57,8 +73,13 @@ namespace ToDo.API.Controllers
         /// <summary>
         /// Get ToDo item
         /// </summary>
-        /// <returns></returns>
+        /// <param name="id">The item Id</param>
+        /// <returns>Return a todo item</returns>
+        /// <response code="200">Returns the todo item</response>
+        /// <response code="404">If the item doesn't exist</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ActivityResponseDTO>> GetById(int id)
         {
             var activity = await _toDoService.GetActivityByIdAsync(id);
@@ -69,8 +90,13 @@ namespace ToDo.API.Controllers
         /// <summary>
         /// Delete ToDo item
         /// </summary>
+        /// <param name="id">The item Id</param>
         /// <returns></returns>
+        /// <response code="204">No content</response>
+        /// <response code="404">If the item doesn't exist</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Delete(int id)
         {
             await _toDoService.DeleteActivityAsync(id);
@@ -80,7 +106,11 @@ namespace ToDo.API.Controllers
         /// <summary>
         /// Update Todo item
         /// </summary>
+        /// <param name="id">The item Id</param>
+        /// <param name="requestModel">The Request model</param>
         /// <returns></returns>
+        /// <response code="204">No content</response>
+        /// <response code="404">If the item doesn't exist</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, ActivityRequestDTO requestModel)
         {
